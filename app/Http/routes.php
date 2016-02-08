89a252c2f514af928e3a43ba0bce5e9fafdcb4af
Marcos Navarro <marcoscnavarro@gmail.com>
2016-02-08 08:41:48 -0300

    Commit da fase 2 do projeto

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Welcome@index');
 
 
 Route::get('/exemplo', function () {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Marcos Navarro</div>
+{{$teste}}
             </div>
         </div>
     </body>
